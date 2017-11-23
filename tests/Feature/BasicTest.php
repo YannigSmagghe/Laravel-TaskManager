a9618d69cd56e229d4ec025dfd32e4661e70afd4
mariecgs <marie.cogis@gmail.com>
2017-11-23 15:28:44 +0100

    WALLAH

--- tests/Feature/BasicTest.php
+++ tests/Feature/BasicTest.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class BasicTest extends DuskTestCase
+class BasicTest extends TestCase
 {
     static $browse;
     /**
