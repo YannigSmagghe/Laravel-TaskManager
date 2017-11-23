796ca80fab4243270262ca020dd0286b26401c1d
stankk <wowstank@hotmail.fr>
2017-11-23 17:10:10 +0100

    remove identifiant git

--- .rocketeer/scm.php
+++ .rocketeer/scm.php
@@ -17,8 +17,8 @@ return [
     // In other cases you can leave this empty too, and you will
     // be prompted for the credentials on deploy. If you don't want
     // to be prompted (public repo, etc) set the values to null
-    'username'   => 'stankk',
-    'password'   => '1GZ%Em4V',
+    'username'   => '',
+    'password'   => '',
 
     // The branch to deploy
     'branch'     => 'master',
